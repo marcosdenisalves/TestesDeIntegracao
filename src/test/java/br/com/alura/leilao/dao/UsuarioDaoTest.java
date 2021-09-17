0bf62599b8b008a274c7a3d2905b54d8b563c17d
Denis Alves <dee.alves2@gmail.com>
2021-09-16 22:08:19 -0300

    Refactoring test code

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,9 +1,13 @@
 package br.com.alura.leilao.dao;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import br.com.alura.leilao.model.Usuario;
@@ -12,19 +16,36 @@ import br.com.alura.leilao.util.JPAUtil;
 class UsuarioDaoTest {
 
 	private UsuarioDao dao;
+	private Usuario usuario;
+	private EntityManager em;
+	
+	@BeforeEach
+	public void beforeEach() {
+		this.em =  JPAUtil.getEntityManager();
+		this.dao = new UsuarioDao(em);
+		em.getTransaction().begin();
+		usuario = criarUsuario();
+	}
+	
+	@AfterEach
+	public void afterEach() {
+		em.getTransaction().rollback();
+	}
 	
 	@Test
-	void testeBuscaDeUsuarioPeloUsername() {
-		EntityManager em = JPAUtil.getEntityManager();
-
+	void deveriaEncontrarUsuarioCadastrado() {
+		assertNotNull(this.dao.buscarPorUsername(usuario.getNome()));
+	}
+	
+	@Test
+	void naoDeveriaEncontrarUsuarioNaoCadastrado() {
+		assertThrows(NoResultException.class ,
+				() -> this.dao.buscarPorUsername("beltrano"));
+	}
+	
+	private Usuario criarUsuario() {
 		Usuario usuario = new Usuario("fulano", "fulano@gmail.com", "12345678");
-		em.getTransaction().begin();
 		em.persist(usuario);
-		em.getTransaction().commit();
-		
-		this.dao = new UsuarioDao(em);
-		Usuario encontrado = this.dao.buscarPorUsername(usuario.getNome());
-		
-		assertNotNull(encontrado);
+		return usuario;
 	}
 }
